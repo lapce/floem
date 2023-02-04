eee928c66064416c4664be432b3cdae5c78b24e7
Dongdong Zhou <dzhou121@gmail.com>
2023-02-04 21:27:45 +0000

    add container

---
+++ src/views/container.rs
@@ -0,0 +1,42 @@
+use crate::{app::AppContext, id::Id, view::View};
+
+pub struct Container<V: View> {
+    id: Id,
+    child: V,
+}
+
+pub fn container<V: View>(cx: AppContext, child: impl Fn(AppContext) -> V) -> Container<V> {
+    let id = cx.new_id();
+    let mut child_cx = cx;
+    child_cx.id = id;
+    let child = child(child_cx);
+    Container { id, child }
+}
+
+impl<V: View> View for Container<V> {
+    fn id(&self) -> Id {
+        self.id
+    }
+
+    fn update(
+        &mut self,
+        cx: &mut crate::context::UpdateCx,
+        id_path: &[Id],
+        state: Box<dyn std::any::Any>,
+    ) -> crate::view::ChangeFlags {
+        let id_path = &id_path[1..];
+        self.child.update(cx, id_path, state)
+    }
+
+    fn layout(&mut self, cx: &mut crate::context::LayoutCx) -> taffy::prelude::Node {
+        cx.layout_node(self.id, true, |cx| vec![self.child.layout(cx)])
+    }
+
+    fn event(&mut self, cx: &mut crate::context::EventCx, event: crate::event::Event) {
+        self.child.event(cx, event);
+    }
+
+    fn paint(&mut self, cx: &mut crate::context::PaintCx) {
+        self.child.paint_main(cx);
+    }
+}
--- src/views/mod.rs
+++ src/views/mod.rs
@@ -1,8 +1,14 @@
 mod label;
 pub use label::*;
+
 mod list;
 pub use list::*;
+
 mod svg;
 pub use svg::*;
+
+mod container;
+pub use container::*;
+
 mod decorator;
 pub use decorator::*;
