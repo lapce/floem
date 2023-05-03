354514f326fdfdf93f02f034c7e6624e418a8a04
presiyan-ivanov <15377841+presiyan-ivanov@users.noreply.github.com>
2023-05-03 21:03:33 +0300

    Remove unused imports and variables in text input. Add virtual list t… (#20)
    * Remove unused imports and variables in text input. Add virtual list to widget gallery and move widgets to separate functions
    
    * Rename *_widget to *_view

--- examples/widget-gallery/src/main.rs
+++ examples/widget-gallery/src/main.rs
@@ -1,66 +1,110 @@
 use floem::{
     peniko::Color,
-    reactive::create_rw_signal,
+    reactive::{create_rw_signal, create_signal, SignalGet},
     style::{JustifyContent, Style},
     view::View,
-    views::{click, label, stack, text_input, Decorators},
+    views::{
+        click, label, scroll, stack, text_input, virtual_list, Decorators, VirtualListDirection,
+        VirtualListItemSize,
+    },
     AppContext,
 };
 
-fn app_view(cx: AppContext) -> impl View {
+fn widg_cont_style() -> Style {
+    Style::BASE
+        .items_center()
+        .margin_bottom(25.0)
+        .justify_content(Some(JustifyContent::SpaceBetween))
+}
+
+
+fn label_view(cx: AppContext) -> impl View {
+    stack(cx, |cx| {
+        (
+            label(cx, move || "Label:".to_owned()).style(cx, || Style::BASE.margin_right(50.0)),
+            label(cx, move || "This is a label".to_owned()),
+        )
+    })
+    .style(cx, || widg_cont_style())
+}
+
+fn button_view(cx: AppContext) -> impl View {
+    stack(cx, |cx| {
+        (
+            label(cx, move || "Button:".to_owned()).style(cx, || Style::BASE.margin_right(50.0)),
+            click(
+                cx,
+                |cx| label(cx, || "Click me".to_string()),
+                move || println!("Button clicked"),
+            )
+            .style(cx, || {
+                Style::BASE.border(1.0).border_radius(10.0).padding(10.0)
+            }),
+        )
+    })
+    .style(cx, || widg_cont_style())
+}
+
+fn text_input_view(cx: AppContext) -> impl View {
     let text = create_rw_signal(cx.scope, "".to_string());
 
-    fn widget_cont_style() -> Style {
-        Style::BASE
-            .items_center()
-            .margin_bottom(20.0)
-            .justify_content(Some(JustifyContent::SpaceBetween))
-    }
+    stack(cx, |cx| {
+        (
+            label(cx, move || "Text input:".to_owned())
+                .style(cx, || Style::BASE.margin_right(50.0)),
+            text_input(cx, text)
+                .style(cx, || {
+                    Style::BASE
+                        .border(1.5)
+                        .background(Color::rgb8(224, 224, 224))
+                        .border_radius(15.0)
+                        .border_color(Color::rgb8(189, 189, 189))
+                        .padding(10.0)
+                })
+                .hover_style(cx, || Style::BASE.border_color(Color::rgb8(66, 66, 66)))
+                .focus_style(cx, || Style::BASE.border_color(Color::LIGHT_SKY_BLUE)),
+        )
+    })
+    .style(cx, || widg_cont_style())
+}
 
+fn virt_list_view(cx: AppContext) -> impl View {
+    let long_list: im::Vector<i32> = (0..1000000).into_iter().collect();
+    let (long_list, _set_long_list) = create_signal(cx.scope, long_list);
+
+    stack(cx, move |cx| {
+        (
+            label(cx, move || "List:".to_owned()).style(cx, || Style::BASE.margin_right(50.0)),
+            scroll(cx, move |cx| {
+                virtual_list(
+                    cx,
+                    VirtualListDirection::Vertical,
+                    move || long_list.get(),
+                    move |item| *item,
+                    move |cx, item| {
+                        label(cx, move || item.to_string())
+                            .style(cx, || Style::BASE.height_px(20.0))
+                    },
+                    VirtualListItemSize::Fixed(20.0),
+                )
+                .style(cx, || Style::BASE.flex_col())
+            })
+            .style(cx, || {
+                Style::BASE.width_px(100.0).height_px(100.0).border(1.0)
+            }),
+        )
+    })
+    .style(cx, || widg_cont_style())
+}
+
+fn app_view(cx: AppContext) -> impl View {
     stack(cx, |cx| {
         (stack(cx, |cx| {
             (
-                stack(cx, |cx| {
-                    (
-                        label(cx, move || "Label:".to_owned())
-                            .style(cx, || Style::BASE.margin_right(50.0)),
-                        label(cx, move || "This is a label".to_owned()),
-                    )
-                })
-                .style(cx, || widget_cont_style()),
-                stack(cx, |cx| {
-                    (
-                        label(cx, move || "Button:".to_owned())
-                            .style(cx, || Style::BASE.margin_right(50.0)),
-                        click(
-                            cx,
-                            |cx| label(cx, || "Click me".to_string()),
-                            move || println!("Button clicked"),
-                        )
-                        .style(cx, || {
-                            Style::BASE.border(1.0).border_radius(10.0).padding(10.0)
-                        }),
-                    )
-                })
-                .style(cx, || widget_cont_style()),
-                stack(cx, |cx| {
-                    (
-                        label(cx, move || "Text input:".to_owned())
-                            .style(cx, || Style::BASE.margin_right(50.0)),
-                        text_input(cx, text)
-                            .style(cx, || {
-                                Style::BASE
-                                    .border(1.5)
-                                    .background(Color::rgb8(224, 224, 224))
-                                    .border_radius(15.0)
-                                    .border_color(Color::rgb8(189, 189, 189))
-                                    .padding(10.0)
-                            })
-                            .hover_style(cx, || Style::BASE.border_color(Color::rgb8(66, 66, 66)))
-                            .focus_style(cx, || Style::BASE.border_color(Color::LIGHT_SKY_BLUE)),
-                    )
-                })
-                .style(cx, || widget_cont_style()),
+                label_view(cx),
+                button_view(cx),
+                text_input_view(cx),
+                virt_list_view(cx),
             )
         })
         .style(cx, || {
