0f5de71fec738ef71eb0787bfa14bf57971cf035
tk103331 <tk103331@gmail.com>
2023-05-05 15:14:32 +0800

    refactor(examples): widget-gallery using tabs for classification

--- examples/widget-gallery/src/main.rs
+++ examples/widget-gallery/src/main.rs
@@ -1,19 +1,20 @@
 use floem::{
     peniko::Color,
-    reactive::{create_rw_signal, create_signal, SignalGet},
-    style::{JustifyContent, Style},
+    reactive::{create_rw_signal, create_signal, SignalGet, SignalUpdate},
+    style::{JustifyContent, Style, CursorStyle},
     view::View,
     views::{
         label, scroll, stack, text_input, virtual_list, Decorators, VirtualListDirection,
-        VirtualListItemSize,
+        VirtualListItemSize, tab, list, container, container_box,
     },
-    AppContext,
+    AppContext, cosmic_text::{Style as FontStyle, Weight},
 };
 
 fn widg_cont_style() -> Style {
     Style::BASE
-        .items_center()
+        .items_start()
         .margin_bottom(25.0)
+        .dimension_pct(1.0, 1.0)
         .justify_content(Some(JustifyContent::SpaceBetween))
 }
 
@@ -97,31 +98,94 @@ fn virt_list_view(cx: AppContext) -> impl View {
 }
 
 fn app_view(cx: AppContext) -> impl View {
+    let tabs: im::Vector<&str> = vec!["Label", "Button", "Input", "List"].into_iter().collect();
+    let (tabs, _set_tabs) = create_signal(cx.scope, tabs);
+
+    let (active_tab, set_active_tab) = create_signal(cx.scope, 0);
     stack(cx, |cx| {
-        (stack(cx, |cx| {
-            (
-                label_view(cx),
-                button_view(cx),
-                text_input_view(cx),
-                virt_list_view(cx),
-            )
-        })
-        .style(cx, || {
-            Style::BASE
-                .background(Color::WHITE_SMOKE)
-                .padding_horiz(50.0)
-                .padding_vert(20.0)
-                .width_px(450.0)
-                .flex_col()
-                .justify_content(Some(JustifyContent::SpaceBetween))
-        }),)
-    })
-    .style(cx, || {
-        Style::BASE
-            .dimension_pct(1.0, 1.0)
-            .justify_center()
-            .items_center()
-    })
+        (
+            container(cx, move |cx| {
+                scroll(cx, move |cx| {
+                    virtual_list(
+                        cx,
+                        VirtualListDirection::Vertical,
+                        move || tabs.get(),
+                        move |item| *item,
+                        move |cx, item| {
+                            let index = tabs.get().iter().position(|it| *it == item).unwrap();
+                            container(cx, move |cx| {
+                                label(cx, move || item.to_string())
+                                .style(cx, || Style::BASE.font_size(24.0))
+                            }) 
+                            .on_click(move |_| {
+                                set_active_tab.update( |v| {
+                                    *v = tabs.get().iter().position(|it| *it == item).unwrap();
+                                });
+                                true
+                            }).style(cx, move || 
+                                Style::BASE.width_pct(1.0).height_px(32.0).padding(2.0).flex_row().justify_center()
+                                .apply_if(index == active_tab.get(), |s| {s.background(Color::GRAY)} )
+                            )
+                            .hover_style(cx, || Style::BASE.background(Color::LIGHT_GRAY).cursor(CursorStyle::Pointer))
+                        },
+                        VirtualListItemSize::Fixed(20.0),
+                    )
+                    .style(cx, || Style::BASE.flex_col())
+                })
+                .style(cx, || {
+                    Style::BASE.dimension_pct(1.0, 1.0).border(1.0).border_color(Color::GRAY)
+                })
+            })
+            .style(cx, || {
+                Style::BASE
+                    .height_pct(1.0)
+                    .width_px(150.0)
+                    .padding_vert(5.0)
+                    .padding_horiz(5.0)
+                    .flex_col()
+                    .items_center()
+            }),
+            container(cx, move |cx| {
+                scroll(cx, move |cx| {
+                    tab(cx, 
+                        move || { active_tab.get() }, 
+                        move || { tabs.get() }, 
+                    |it| { *it }, 
+                    |cx, it|  {
+                        match it {
+                            "Label" => container_box(cx, |cx| {
+                                Box::new(label_view(cx))
+                            }),
+                            "Button" => container_box(cx, |cx| {
+                                Box::new(button_view(cx))
+                            }),
+                            "Input" => container_box(cx, |cx| {
+                                Box::new(text_input_view(cx))
+                            }),
+                            "List" => container_box(cx, |cx| {
+                                Box::new(virt_list_view(cx))
+                            }),
+                            _ => container_box(cx, |cx| {
+                                Box::new(label(cx, || "Not implemented".to_owned()))
+                            }),
+                        }
+                    }).style(cx, || Style::BASE.dimension_pct(1.0, 1.0))
+                })
+                .style(cx, || {
+                    Style::BASE.dimension_pct(1.0, 1.0).border(1.0).border_color(Color::GRAY)
+                })
+            })
+            .style(cx, || {
+                Style::BASE
+                    .dimension_pct(1.0, 1.0)
+                    .padding_vert(5.0)
+                    .padding_horiz(5.0)
+                    .flex_col()
+                    .items_center()
+            }),
+        )
+    }).style(cx, || Style::BASE.dimension_pct(1.0, 1.0))
+
 }
 
 fn main() {
