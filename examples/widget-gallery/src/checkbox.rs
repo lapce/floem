7f13520e0c5826afa6215a44f918c251224412cc
tk <tk103331@gmail.com>
2023-05-10 03:43:26 +0800

    chore(examples): add disabeld input and checkbox (#33)

--- examples/widget-gallery/src/checkbox.rs
+++ examples/widget-gallery/src/checkbox.rs
@@ -36,6 +36,22 @@ pub fn checkbox_view() -> impl View {
                     true
                 })
             }),
+            form_item("Disabled Checkbox:".to_string(), 120.0, move || {
+                stack(|| {
+                    (
+                        checkbox(is_checked).focus_visible_style(|| {
+                            Style::BASE.border_color(Color::BLUE).border(2.)
+                        }),
+                        label(|| "Check me!".to_string()),
+                    )
+                })
+                .style(|| Style::BASE.color(Color::GRAY))
+                .disabled(|| true)
+                .on_click(move |_| {
+                    set_is_checked.update(|checked| *checked = !*checked);
+                    true
+                })
+            }),
         )
     })
 }
