8f00e4d530441982627dd62cbf20f2e3f0efac6d
Jared Moulton <jaredmoulton3@gmail.com>
2023-05-13 12:46:46 -0600

    Implment View for () for an empty type (#36)
    * Implment View for () for an empty type
    
    This is done in order to have an empy type that implements view. This
    makes it so that a container can have nothing inside of it but can still
    display properties like border. This is useful for creating shapes.
    
    * Change empty to a struct with an ID

---
+++ src/views/empty.rs
@@ -0,0 +1,58 @@
+use crate::{
+    app_handle::AppContext,
+    id::Id,
+    view::{ChangeFlags, View},
+};
+
+pub struct Empty {
+    id: Id,
+}
+
+pub fn empty() -> Empty {
+    let cx = AppContext::get_current();
+    let id = cx.new_id();
+    Empty { id }
+}
+
+impl View for Empty {
+    fn id(&self) -> Id {
+        self.id
+    }
+
+    fn child(&mut self, _id: Id) -> Option<&mut dyn View> {
+        None
+    }
+
+    fn children(&mut self) -> Vec<&mut dyn View> {
+        Vec::new()
+    }
+
+    fn debug_name(&self) -> std::borrow::Cow<'static, str> {
+        "Empty".into()
+    }
+
+    fn update(
+        &mut self,
+        _cx: &mut crate::context::UpdateCx,
+        _state: Box<dyn std::any::Any>,
+    ) -> crate::view::ChangeFlags {
+        ChangeFlags::empty()
+    }
+
+    fn layout(&mut self, cx: &mut crate::context::LayoutCx) -> taffy::prelude::Node {
+        cx.layout_node(self.id, false, |_| Vec::new())
+    }
+
+    fn compute_layout(&mut self, _cx: &mut crate::context::LayoutCx) {}
+
+    fn event(
+        &mut self,
+        _cx: &mut crate::context::EventCx,
+        _id_path: Option<&[Id]>,
+        _event: crate::event::Event,
+    ) -> bool {
+        false
+    }
+
+    fn paint(&mut self, _cx: &mut crate::context::PaintCx) {}
+}
