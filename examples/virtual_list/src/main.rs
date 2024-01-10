ad431b86e0d50e1e9a144b70ca9b3208d50c54c5
John Kåre Alsaker <john.kare.alsaker@gmail.com>
2024-01-10 19:22:05 +0100

    Fix the virtual_list example

--- examples/virtual_list/src/main.rs
+++ examples/virtual_list/src/main.rs
@@ -2,9 +2,9 @@ use floem::{
     reactive::create_signal,
     unit::UnitExt,
     view::View,
-    views::virtual_stack,
     views::Decorators,
     views::{container, label, scroll, VirtualDirection, VirtualItemSize},
+    widgets::virtual_list,
 };
 
 fn app_view() -> impl View {
@@ -13,14 +13,14 @@ fn app_view() -> impl View {
 
     container(
         scroll(
-            virtual_stack(
+            virtual_list(
                 VirtualDirection::Vertical,
                 VirtualItemSize::Fixed(Box::new(|| 20.0)),
                 move || long_list.get(),
                 move |item| *item,
                 move |item| label(move || item.to_string()).style(|s| s.height(20.0)),
             )
-            .style(|s| s.flex_col()),
+            .style(|s| s.flex_col().width_full()),
         )
         .style(|s| s.width(100.0).height(100.pct()).border(1.0)),
     )
