62b643a6ce27de4bfd63350c4cc454942ce6eb3b
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2024-03-05 20:35:14 +1000

    added more docs, fixed docs test, found more typos

--- reactive/src/context.rs
+++ reactive/src/context.rs
@@ -6,10 +6,16 @@ use crate::runtime::RUNTIME;
 /// You can store a Context value anywhere, and retrieve it from anywhere afterwards.
 ///
 /// # Example
-///
+/// In a parent component:
+/// ```rust
+/// # use floem_reactive::provide_context;
+/// provide_context(42);
+/// provide_context(String::from("Hello world"));
 /// ```
-/// use floem::reactive::use_context;
 ///
+/// And so in a child component you can retrieve each context data by specifying the type:
+/// ```rust
+/// # use floem_reactive::use_context;
 /// let foo: Option<i32> = use_context();
 /// let bar: Option<String> = use_context();
 /// ```
@@ -32,12 +38,18 @@ where
 /// The stored context value can be retrieved from anywhere by using [use_context](use_context)
 ///
 /// # Example
-///
-/// ```
-/// use floem::reactive::provide_context;
-///
+/// In a parent component:
+/// ```rust
+/// # use floem_reactive::provide_context;
 /// provide_context(42);
 /// provide_context(String::from("Hello world"));
+/// ```
+///
+/// And so in a child component you can retrieve each context data by specifying the type:
+/// ```rust
+/// # use floem_reactive::use_context;
+/// let foo: Option<i32> = use_context();
+/// let bar: Option<String> = use_context();
 /// ```
 pub fn provide_context<T>(value: T)
 where
