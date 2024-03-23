942b35a2aaec3414131ec1d4e7f096f699e7038f
Long0x0 <51022287+Long0x0@users.noreply.github.com>
2024-03-24 03:16:57 +0800

    fix: ignore synthetic keyboard input (#387)
    Co-authored-by: long <gmchenlong@gmail.com>

--- src/app_handle.rs
+++ src/app_handle.rs
@@ -173,8 +173,14 @@ impl ApplicationHandle {
             WindowEvent::Focused(focused) => {
                 window_handle.focused(focused);
             }
-            WindowEvent::KeyboardInput { event, .. } => {
-                window_handle.key_event(event);
+            WindowEvent::KeyboardInput {
+                event,
+                is_synthetic,
+                ..
+            } => {
+                if !is_synthetic {
+                    window_handle.key_event(event);
+                }
             }
             WindowEvent::ModifiersChanged(modifiers) => {
                 window_handle.modifiers = modifiers.state();
