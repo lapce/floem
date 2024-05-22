737350f85c5bd015f20238c55324394387aeb1fe
Joshua Padman <joshua@padman.id.au>
2024-05-22 21:54:03 +1000

    PNG and SVG examples - Updated assets
    The original icon.png had unknown origins, prior to inclusion in winit.
    Replaced with assets that were already in the repository and do not
    require attribution per https://github.com/lapce/floem/pull/116#issuecomment-1752165921

--- examples/window-icon/src/main.rs
+++ examples/window-icon/src/main.rs
@@ -1,15 +1,51 @@
 use floem::{
+    close_window,
     event::{Event, EventListener},
     keyboard::{Key, NamedKey},
     kurbo::Size,
-    views::Decorators,
-    window::{Icon, WindowConfig},
+    new_window,
+    views::{button, label, v_stack, Decorators},
+    window::{Icon, WindowConfig, WindowId},
     Application, IntoView, View,
 };
 use std::path::Path;
 
+fn sub_window_view(id: WindowId) -> impl IntoView {
+    v_stack((
+        label(move || String::from("This window has an icon from an SVG file."))
+            .style(|s| s.font_size(30.0)),
+        button(|| "Close this window").on_click_stop(move |_| {
+            close_window(id);
+        }),
+    ))
+    .style(|s| {
+        s.flex_col()
+            .items_center()
+            .justify_center()
+            .width_full()
+            .height_full()
+            .gap(0.0, 10.0)
+    })
+}
+
 fn app_view() -> impl IntoView {
-    let view = "Show Window Icon".style(|s| s.font_size(30.0)).style(|s| {
+    let view = v_stack((
+        label(move || String::from("This window has an icon from a PNG file"))
+            .style(|s| s.font_size(30.0)),
+        button(|| "Open another window").on_click_stop(|_| {
+            let svg_icon = load_svg_icon(include_str!("../assets/ferris.svg"));
+            new_window(
+                sub_window_view,
+                Some(
+                    WindowConfig::default()
+                        .size(Size::new(600.0, 150.0))
+                        .title("Window Icon Sub Example")
+                        .window_icon(svg_icon),
+                ),
+            );
+        }),
+    ))
+    .style(|s| {
         s.flex_col()
             .items_center()
             .justify_center()
@@ -29,8 +65,8 @@ fn app_view() -> impl IntoView {
 }
 
 fn main() {
-    let path = concat!(env!("CARGO_MANIFEST_DIR"), "./assets/icon.png");
-    let icon = load_icon(Path::new(path));
+    let png_icon_path = concat!(env!("CARGO_MANIFEST_DIR"), "./assets/ferris.png");
+    let png_icon = load_png_icon(Path::new(png_icon_path));
 
     Application::new()
         .window(
@@ -38,14 +74,14 @@ fn main() {
             Some(
                 WindowConfig::default()
                     .size(Size::new(800.0, 250.0))
-                    .title("Window Size Example")
-                    .window_icon(icon),
+                    .title("Window Icon Example")
+                    .window_icon(png_icon),
             ),
         )
         .run();
 }
 
-fn load_icon(path: &Path) -> Icon {
+fn load_png_icon(path: &Path) -> Icon {
     let (icon_rgba, icon_width, icon_height) = {
         let image = image::open(path)
             .expect("Failed to open icon path")
@@ -54,5 +90,11 @@ fn load_icon(path: &Path) -> Icon {
         let rgba = image.into_raw();
         (rgba, width, height)
     };
+    Icon::from_rgba(icon_rgba, icon_width, icon_height).expect("Failed to open icon")
+}
+
+fn load_svg_icon(svg: &str) -> Icon {
+    let svg = nsvg::parse_str(svg, nsvg::Units::Pixel, 96.0).unwrap();
+    let (icon_width, icon_height, icon_rgba) = svg.rasterize_to_raw_rgba(1.0).unwrap();
     Icon::from_rgba(icon_rgba, icon_width, icon_height).expect("Failed to open icon")
 }
