4c22b876850478be1a0de3fffdc2820a2e690ce9
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2024-05-28 03:32:36 +1000

    added an example to demystify stacks

---
+++ examples/stacks/src/dyn_stack.rs
@@ -0,0 +1,29 @@
+use floem::{
+    reactive::create_rw_signal,
+    views::{dyn_stack, scroll, ButtonClass, Decorators},
+    IntoView,
+};
+
+pub fn dyn_stack_view() -> impl IntoView {
+    // With the dyn_stack you can change the stack at runtime by controlling
+    // your stack with a signal.
+
+    let long_list: im::Vector<i32> = (0..10).collect();
+    let long_list = create_rw_signal(long_list);
+
+    (
+        "Add an item".class(ButtonClass).on_click_stop(move |_| {
+            long_list.update(|list| list.push_front(list.len() as i32 + 1))
+        }),
+        scroll(
+            dyn_stack(
+                move || long_list.get(),
+                move |item| *item,
+                move |item| item.style(|s| s.height(20).justify_center()),
+            )
+            .style(|s| s.flex_col().width_full()),
+        )
+        .style(|s| s.width(100).height(200).border(1)),
+    )
+        .style(|s| s.flex_col().gap(0, 5).margin_top(10))
+}
---
+++ examples/stacks/src/stack_from_iter.rs
@@ -0,0 +1,14 @@
+use floem::{
+    views::{stack_from_iter, Decorators},
+    IntoView,
+};
+
+pub fn stack_from_iter_view() -> impl IntoView {
+    // You can also use v_stack_from_iter and h_stack_from_iter for built in
+    // flex direction.
+
+    let collection: Vec<usize> = (0..10).collect();
+
+    stack_from_iter(collection.iter().map(|val| format!("Item {}", val)))
+        .style(|s| s.flex_col().gap(0, 5).margin_top(10))
+}
