ec82eb5117e1844d31ed7f7b23c007b3cada1abd
jrmoulton <jaredmoulton3@gmail.com>
2024-09-12 02:54:14 -0600

    fix ops impls

--- reactive/src/impls.rs
+++ reactive/src/impls.rs
@@ -1,123 +1,83 @@
 use crate::{Memo, ReadSignal, RwSignal, SignalUpdate, SignalWith, WriteSignal};
 
-// Macro for implementing binary operations (both int and float)
+// Binary operation macro (unchanged)
 macro_rules! impl_bin_op {
-    ($signal_type:ident, $trait:ident, $method:ident, $op:tt, $($type:ty),+) => {
-        $(
-            impl std::ops::$trait<$type> for $signal_type<$type> {
-                type Output = $type;
-                fn $method(self, rhs: $type) -> Self::Output {
-                    self.with(|val| *val $op rhs)
-                }
+    ($signal_type:ident, $trait:ident, $method:ident, $op:tt) => {
+        impl<T: std::ops::$trait<Output = T> + Clone + 'static> std::ops::$trait<T> for $signal_type<T>
+        where
+            for<'a> &'a T: std::ops::$trait<T, Output = T>
+        {
+            type Output = T;
+            fn $method(self, rhs: T) -> Self::Output {
+                self.with(|val| (*val).clone() $op rhs)
             }
-        )+
+        }
     };
 }
 
-// Macro for implementing integer-only binary operations
-macro_rules! impl_int_bin_op {
-    ($signal_type:ident, $trait:ident, $method:ident, $op:tt, $($type:ty),+) => {
-        $(
-            impl std::ops::$trait<$type> for $signal_type<$type> {
-                type Output = $type;
-                fn $method(self, rhs: $type) -> Self::Output {
-                    self.with(|val| *val $op rhs)
-                }
-            }
-        )+
-    };
-}
-
-// Macro for implementing assignment operations (both int and float)
+// Assignment operation macro (unchanged)
 macro_rules! impl_assign_op {
-    ($signal_type:ident, $trait:ident, $method:ident, $op:tt, $($type:ty),+) => {
-        $(
-            impl std::ops::$trait<$type> for $signal_type<$type> {
-                fn $method(&mut self, rhs: $type) {
-                    self.update(|val| *val $op rhs);
-                }
+    ($signal_type:ident, $trait:ident, $method:ident, $op:tt) => {
+        impl<T: std::ops::$trait + 'static> std::ops::$trait<T> for $signal_type<T> {
+            fn $method(&mut self, rhs: T) {
+                self.update(|val| *val $op rhs);
             }
-        )+
+        }
     };
 }
 
-// Macro for implementing integer-only assignment operations
-macro_rules! impl_int_assign_op {
-    ($signal_type:ident, $trait:ident, $method:ident, $op:tt, $($type:ty),+) => {
-        $(
-            impl std::ops::$trait<$type> for $signal_type<$type> {
-                fn $method(&mut self, rhs: $type) {
-                    self.update(|val| *val $op rhs);
-                }
-            }
-        )+
-    };
-}
-
-// Macro for implementing PartialEq
+// PartialEq implementation macro (unchanged)
 macro_rules! impl_partial_eq {
-    ($signal_type:ident, $($type:ty),+) => {
-        $(
-            impl PartialEq<$type> for $signal_type<$type> {
-                fn eq(&self, other: &$type) -> bool {
-                    self.with(|val| *val == *other)
-                }
+    ($signal_type:ident) => {
+        impl<T: PartialEq + 'static> PartialEq<T> for $signal_type<T> {
+            fn eq(&self, other: &T) -> bool {
+                self.with(|val| *val == *other)
             }
-        )+
+        }
     };
 }
 
-// Macro for operations that use 'with' (binary ops and PartialEq)
+// Macro for implementing all binary operations and PartialEq
 macro_rules! impl_with_ops {
-    ($signal_type:ident, $($int_type:ty),+; $($float_type:ty),+) => {
-        // Operations for both integer and float types
-        impl_bin_op!($signal_type, Add, add, +, $($int_type,)+ $($float_type),+);
-        impl_bin_op!($signal_type, Sub, sub, -, $($int_type,)+ $($float_type),+);
-        impl_bin_op!($signal_type, Mul, mul, *, $($int_type,)+ $($float_type),+);
-        impl_bin_op!($signal_type, Div, div, /, $($int_type,)+ $($float_type),+);
-
-        // Operations only for integer types
-        impl_int_bin_op!($signal_type, Rem, rem, %, $($int_type),+);
-        impl_int_bin_op!($signal_type, BitAnd, bitand, &, $($int_type),+);
-        impl_int_bin_op!($signal_type, BitOr, bitor, |, $($int_type),+);
-        impl_int_bin_op!($signal_type, BitXor, bitxor, ^, $($int_type),+);
-        impl_int_bin_op!($signal_type, Shl, shl, <<, $($int_type),+);
-        impl_int_bin_op!($signal_type, Shr, shr, >>, $($int_type),+);
-
-        // PartialEq for both integer and float types
-        impl_partial_eq!($signal_type, $($int_type,)+ $($float_type),+);
+    ($signal_type:ident) => {
+        impl_bin_op!($signal_type, Add, add, +);
+        impl_bin_op!($signal_type, Sub, sub, -);
+        impl_bin_op!($signal_type, Mul, mul, *);
+        impl_bin_op!($signal_type, Div, div, /);
+        impl_bin_op!($signal_type, Rem, rem, %);
+        impl_bin_op!($signal_type, BitAnd, bitand, &);
+        impl_bin_op!($signal_type, BitOr, bitor, |);
+        impl_bin_op!($signal_type, BitXor, bitxor, ^);
+        impl_bin_op!($signal_type, Shl, shl, <<);
+        impl_bin_op!($signal_type, Shr, shr, >>);
+        impl_partial_eq!($signal_type);
     };
 }
 
-// Macro for assignment operations
+// Macro for implementing all assignment operations
 macro_rules! impl_assign_ops {
-    ($signal_type:ident, $($int_type:ty),+; $($float_type:ty),+) => {
-        // Operations for both integer and float types
-        impl_assign_op!($signal_type, AddAssign, add_assign, +=, $($int_type,)+ $($float_type),+);
-        impl_assign_op!($signal_type, SubAssign, sub_assign, -=, $($int_type,)+ $($float_type),+);
-        impl_assign_op!($signal_type, MulAssign, mul_assign, *=, $($int_type,)+ $($float_type),+);
-        impl_assign_op!($signal_type, DivAssign, div_assign, /=, $($int_type,)+ $($float_type),+);
-
-        // Operations only for integer types
-        impl_int_assign_op!($signal_type, RemAssign, rem_assign, %=, $($int_type),+);
-        impl_int_assign_op!($signal_type, BitAndAssign, bitand_assign, &=, $($int_type),+);
-        impl_int_assign_op!($signal_type, BitOrAssign, bitor_assign, |=, $($int_type),+);
-        impl_int_assign_op!($signal_type, BitXorAssign, bitxor_assign, ^=, $($int_type),+);
-        impl_int_assign_op!($signal_type, ShlAssign, shl_assign, <<=, $($int_type),+);
-        impl_int_assign_op!($signal_type, ShrAssign, shr_assign, >>=, $($int_type),+);
-
+    ($signal_type:ident) => {
+        impl_assign_op!($signal_type, AddAssign, add_assign, +=);
+        impl_assign_op!($signal_type, SubAssign, sub_assign, -=);
+        impl_assign_op!($signal_type, MulAssign, mul_assign, *=);
+        impl_assign_op!($signal_type, DivAssign, div_assign, /=);
+        impl_assign_op!($signal_type, RemAssign, rem_assign, %=);
+        impl_assign_op!($signal_type, BitAndAssign, bitand_assign, &=);
+        impl_assign_op!($signal_type, BitOrAssign, bitor_assign, |=);
+        impl_assign_op!($signal_type, BitXorAssign, bitxor_assign, ^=);
+        impl_assign_op!($signal_type, ShlAssign, shl_assign, <<=);
+        impl_assign_op!($signal_type, ShrAssign, shr_assign, >>=);
     };
 }
 
-// Combined macro for all operations
 macro_rules! impl_all_ops {
-    ($signal_type:ident, $($int_type:ty),+; $($float_type:ty),+) => {
-        impl_with_ops!($signal_type, $($int_type),+; $($float_type),+);
-        impl_assign_ops!($signal_type, $($int_type),+; $($float_type),+);
+    ($signal_type:ident) => {
+        impl_assign_ops!($signal_type);
+        impl_with_ops!($signal_type);
     };
 }
 
-impl_all_ops!(RwSignal, i32, i16, i8, u64, u32, u16, u8; f32, f64);
-impl_assign_ops!(WriteSignal, i32, i16, i8, u64, u32, u16, u8; f32, f64);
-impl_with_ops!(ReadSignal, i32, i16, i8, u64, u32, u16, u8; f32, f64);
-impl_with_ops!(Memo, i32, i16, i8, u64, u32, u16, u8; f32, f64);
+impl_all_ops!(RwSignal);
+impl_assign_ops!(WriteSignal);
+impl_with_ops!(ReadSignal);
+impl_with_ops!(Memo);
