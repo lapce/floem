c1675c5aa18ee6a584e6e932898c946370b22edf
Daniel Khodyrev <khodyrevds@gmail.com>
2024-12-13 16:15:33 +0700

    issue 403: Editor selection no longer shows selection on empty lines

--- src/views/editor/view.rs
+++ src/views/editor/view.rs
@@ -372,12 +372,12 @@ impl EditorView {
             };
 
             let line_height = f64::from(ed.line_height(line));
-            
+
             // Skip over empty selections
             if !info.is_empty_phantom() && left_col == right_col {
                 let rect = Rect::from_origin_size((0.0, vline_y), (CHAR_WIDTH, line_height));
                 cx.fill(&rect, color, 0.0);
-                
+
                 continue;
             }
 
