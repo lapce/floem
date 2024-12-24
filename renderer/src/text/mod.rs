6f4a88f94e3ebd640937550e92c3b017150a4cf1
fengqi <362254883@qq.com>
2024-12-24 17:18:54 +0800

    fmt

--- renderer/src/text/mod.rs
+++ renderer/src/text/mod.rs
@@ -6,4 +6,4 @@ pub use cosmic_text::{
     fontdb, CacheKey, Cursor, Family, LayoutGlyph, LayoutLine, Stretch, Style, SubpixelBin,
     SwashCache, SwashContent, Weight, Wrap,
 };
-pub use layout::{HitPoint, HitPosition, TextLayout, FONT_SYSTEM, LayoutRun};
+pub use layout::{HitPoint, HitPosition, LayoutRun, TextLayout, FONT_SYSTEM};
