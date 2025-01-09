be075200bcfe4cad0f4f00e5db591ed877d88bfc
anish <85304651+anishsinha-io@users.noreply.github.com>
2025-01-08 22:02:29 -0500

    Enhance checkbox customizability (#728)
    * Enhance checkbox customizability
    
    The `Checkbox` component now offers a greater degree of customizability
    over the appearance of the `checked` state. Previously, the checkmark
    was not customizable. Now, users may provide their own SVGs if they
    wish. No existing APIs were broken, but a few new ones were added,
    namely there's now a `custom_` prefixed static method for each existing
    method. Also, two new convenience functions, `custom_checkbox` and
    `custom_labeled_checkboxes` are available. The default checkbox SVG is
    now made available to users if they want to use it elsewhere, for
    example in a different component (previously it was a function-local
    `const`, now it's a `pub const`.
    
    The `checkbox` example in the `widget-gallery` has been enhanced with
    two additional examples showcasing some of the new behavior.

--- src/views/checkbox.rs
+++ src/views/checkbox.rs
@@ -22,11 +22,23 @@ style_class!(
     pub LabeledCheckboxClass
 );
 
-fn checkbox_svg(checked: impl SignalGet<bool> + 'static) -> impl IntoView {
-    const CHECKBOX_SVG: &str = r#"<svg xmlns="http://www.w3.org/2000/svg" viewBox="-2 -2 16 16"><polygon points="5.19,11.83 0.18,7.44 1.82,5.56 4.81,8.17 10,1.25 12,2.75" /></svg>"#;
-    let svg_str = move || if checked.get() { CHECKBOX_SVG } else { "" }.to_string();
-    svg(CHECKBOX_SVG)
-        .update_value(svg_str)
+/// The default checkbox SVG
+pub const DEFAULT_CHECKBOX_SVG: &str = r#"<svg xmlns="http://www.w3.org/2000/svg" viewBox="-2 -2 16 16"><polygon points="5.19,11.83 0.18,7.44 1.82,5.56 4.81,8.17 10,1.25 12,2.75" /></svg>"#;
+
+fn checkbox_svg(
+    checked: impl SignalGet<bool> + 'static,
+    check_svg: impl Into<String> + Clone + 'static,
+) -> impl IntoView {
+    let check = check_svg.clone();
+    let update_svg = move || {
+        if checked.get() {
+            check_svg.clone().into()
+        } else {
+            "".to_string()
+        }
+    };
+    svg(check)
+        .update_value(update_svg)
         .class(CheckboxClass)
         .keyboard_navigable()
 }
@@ -48,11 +60,24 @@ impl Checkbox {
     ///
     /// You can add an `on_update` handler to the returned `ValueContainer` to handle changes.
     #[allow(clippy::new_ret_no_self)]
+    #[inline]
     pub fn new(checked: impl Fn() -> bool + 'static) -> ValueContainer<bool> {
+        Self::new_custom(checked, DEFAULT_CHECKBOX_SVG)
+    }
+
+    /// Creates a new checkbox with a closure that determines its checked state and accepts a custom SVG
+    ///
+    /// The semantics are the same as [`Checkbox::new`].
+    ///
+    /// You can add an `on_update` handler to the returned `ValueContainer` to handle changes.
+    pub fn new_custom(
+        checked: impl Fn() -> bool + 'static,
+        custom_check: impl Into<String> + Clone + 'static,
+    ) -> ValueContainer<bool> {
         let (inbound_signal, outbound_signal) = create_value_container_signals(checked);
 
         value_container(
-            checkbox_svg(inbound_signal.read_only()).on_click_stop(move |_| {
+            checkbox_svg(inbound_signal.read_only(), custom_check).on_click_stop(move |_| {
                 let checked = inbound_signal.get_untracked();
                 outbound_signal.set(!checked);
             }),
@@ -64,10 +89,21 @@ impl Checkbox {
     ///
     /// This method is ideal when you need a checkbox that not only reflects a signal's state but also updates it.
     /// Clicking the checkbox will toggle its state and update the signal accordingly.
+    #[inline]
     pub fn new_rw(
         checked: impl SignalGet<bool> + SignalUpdate<bool> + Copy + 'static,
     ) -> impl IntoView {
-        checkbox_svg(checked).on_click_stop(move |_| {
+        Self::new_rw_custom(checked, DEFAULT_CHECKBOX_SVG)
+    }
+
+    /// Creates a new checkbox with a signal that provides and updates its checked state and accepts a custom SVG for the symbol.
+    ///
+    /// The semantics are the same as [`Checkbox::new_rw`].
+    pub fn new_rw_custom(
+        checked: impl SignalGet<bool> + SignalUpdate<bool> + Copy + 'static,
+        custom_check: impl Into<String> + Clone + 'static,
+    ) -> impl IntoView {
+        checkbox_svg(checked, custom_check).on_click_stop(move |_| {
             checked.update(|val| *val = !*val);
         })
     }
@@ -76,15 +112,27 @@ impl Checkbox {
     ///
     /// This method is useful when you want a labeled checkbox whose state is determined by a closure.
     /// The label is also provided by a closure, allowing for dynamic updates.
+    #[inline]
     pub fn labeled<S: Display + 'static>(
         checked: impl Fn() -> bool + 'static,
         label: impl Fn() -> S + 'static,
+    ) -> ValueContainer<bool> {
+        Self::custom_labeled(checked, label, DEFAULT_CHECKBOX_SVG)
+    }
+
+    /// Creates a new labeled checkbox with a closure that determines its checked state and accepts a custom SVG for the symbol.
+    ///
+    /// The semantics are the same as [`Checkbox::labeled`].
+    pub fn custom_labeled<S: Display + 'static>(
+        checked: impl Fn() -> bool + 'static,
+        label: impl Fn() -> S + 'static,
+        custom_check: impl Into<String> + Clone + 'static,
     ) -> ValueContainer<bool> {
         let (inbound_signal, outbound_signal) = create_value_container_signals(checked);
 
         value_container(
             h_stack((
-                checkbox_svg(inbound_signal.read_only()),
+                checkbox_svg(inbound_signal.read_only(), custom_check),
                 views::label(label),
             ))
             .class(LabeledCheckboxClass)
@@ -101,11 +149,23 @@ impl Checkbox {
     ///
     /// This method is ideal when you need a labeled checkbox that not only reflects a signal's state but also updates it.
     /// Clicking the checkbox will toggle its state and update the signal accordingly.
+    #[inline]
     pub fn labeled_rw<S: Display + 'static>(
         checked: impl SignalGet<bool> + SignalUpdate<bool> + Copy + 'static,
         label: impl Fn() -> S + 'static,
     ) -> impl IntoView {
-        h_stack((checkbox_svg(checked), views::label(label)))
+        Self::custom_labeled_rw(checked, label, DEFAULT_CHECKBOX_SVG)
+    }
+
+    /// Creates a new labeled checkbox with a signal that provides and updates its checked state and accepts a custom SVG.
+    ///
+    /// The semantics are the same as [`Checkbox::labeled_rw`].
+    pub fn custom_labeled_rw<S: Display + 'static>(
+        checked: impl SignalGet<bool> + SignalUpdate<bool> + Copy + 'static,
+        label: impl Fn() -> S + 'static,
+        custom_check: impl Into<String> + Clone + 'static,
+    ) -> impl IntoView {
+        h_stack((checkbox_svg(checked, custom_check), views::label(label)))
             .class(LabeledCheckboxClass)
             .style(|s| s.items_center().justify_center())
             .on_click_stop(move |_| {
@@ -119,10 +179,27 @@ pub fn checkbox(checked: impl Fn() -> bool + 'static) -> ValueContainer<bool> {
     Checkbox::new(checked)
 }
 
+/// Renders a checkbox using a `checked` signal and custom SVG. See also [`Checkbox::new_rw`] and
+pub fn custom_checkbox(
+    checked: impl Fn() -> bool + 'static,
+    custom_check: impl Into<String> + Clone + 'static,
+) -> ValueContainer<bool> {
+    Checkbox::new_custom(checked, custom_check)
+}
+
 /// Renders a checkbox using the provided checked signal. See also [`Checkbox::labeled`] and [`Checkbox::labeled_rw`].
 pub fn labeled_checkbox<S: Display + 'static>(
     checked: impl Fn() -> bool + 'static,
     label: impl Fn() -> S + 'static,
 ) -> ValueContainer<bool> {
     Checkbox::labeled(checked, label)
+}
+
+/// Renders a checkbox using the a `checked` signal and a custom SVG. See also [`Checkbox::custom_labeled_rw`]
+pub fn custom_labeled_checkbox<S: Display + 'static>(
+    checked: impl Fn() -> bool + 'static,
+    label: impl Fn() -> S + 'static,
+    custom_check: impl Into<String> + Clone + 'static,
+) -> ValueContainer<bool> {
+    Checkbox::custom_labeled(checked, label, custom_check)
 }
