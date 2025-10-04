6aa0bc9598a549d69dc7acc403e02ba82b84f163
Arthur Cosentino <ArthurCose@gmail.com>
2025-10-04 02:30:38 -0400

    Pass affinity calculation from cosmic text to resolve cursor affinity for clicks (#897)

--- renderer/src/text/mod.rs
+++ renderer/src/text/mod.rs
@@ -3,7 +3,7 @@ mod layout;
 
 pub use attrs::{Attrs, AttrsList, AttrsOwned, FamilyOwned, LineHeightValue};
 pub use cosmic_text::{
-    fontdb, Align, CacheKey, Cursor, Family, LayoutGlyph, LayoutLine, LineEnding, Stretch, Style,
-    SubpixelBin, SwashCache, SwashContent, Weight, Wrap,
+    fontdb, Affinity, Align, CacheKey, Cursor, Family, LayoutGlyph, LayoutLine, LineEnding,
+    Stretch, Style, SubpixelBin, SwashCache, SwashContent, Weight, Wrap,
 };
 pub use layout::{HitPoint, HitPosition, LayoutRun, TextLayout, FONT_SYSTEM};
