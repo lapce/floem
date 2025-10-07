5eb13030b0e072a629257ee9c912af03ada96de9
Karol <cg@local.gitea>
2025-10-07 22:25:18 +0100

    chore: add todo on push_clip_layer

--- vello/src/lib.rs
+++ vello/src/lib.rs
@@ -648,6 +648,11 @@ fn common_alpha_mask_scene(
 
     alpha_mask(&mut scene);
 
+    // TODO: change it to `push_clip_layer` once working
+    // scene.push_clip_layer(
+    //     Affine::IDENTITY,
+    //     &Rect::from_origin_size((0., 0.), size)
+    // );
     #[expect(deprecated, reason = "`push_clip_layer is not working for svgs`")]
     scene.push_layer(
         vello::peniko::BlendMode {
